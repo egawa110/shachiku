54eb32b65438a9dda7ffe6541b3b064d4eeb18e6
DESKTOP-M30FNO0\GA1B <h1202419013@hiratagakuen.onmicrosoft.com>
2024-11-06 12:27:21 +0900

    操作説明のシーン追加・ボタンの色変更

--- 死神お仕事！/Assets/Shishido/script/ChangeScene.cs
+++ 死神お仕事！/Assets/Shishido/script/ChangeScene.cs
@@ -6,12 +6,12 @@ using UnityEngine.SceneManagement; // シーン切り替えに必要
 
 public class ChangeScene : MonoBehaviour
 {
-        public string SceneName; // 読み込むシーン名
+    public string SceneName; // 読み込むシーン名
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,9 +22,10 @@ public class ChangeScene : MonoBehaviour
     // シーンを読み込む
     public void Load()
     {
-        FadeManager.Instance.LoadScene(SceneName, 1.0f);
-
         SceneManager.LoadScene(SceneName);
+
+        FadeManager.Instance.LoadScene(SceneName, 0.3f);
+        Debug.Log(SceneName);
 
     }
 }
