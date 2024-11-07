073f66009836ea2a10a6049354a7cf55b7b292e2
yasutomo <h1202415011@hiratagakuen.onmicrosoft.com>
2024-11-07 11:40:07 +0900

    召喚デモ

--- 死神お仕事！/Assets/Hukusumi/Zeere/ZeereBeem1.cs
+++ 死神お仕事！/Assets/Hukusumi/Zeere/ZeereBeem1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZeereBeem2 : MonoBehaviour
+public class ZeereBeem1 : MonoBehaviour
 {
     [SerializeField] GameObject target;
     SpriteRenderer sr;
@@ -10,7 +10,7 @@ public class ZeereBeem2 : MonoBehaviour
     //public GameObject objPrefab;
     public float firetime = 180.0f;//発射
     public float fireSpeed = 0.0f;
-    bool onoff = false;
+    bool onoff = false;//起動用
     float transparencyON = 0.0f;//透明
     float transparencyOFF = 1.0f;//不透明
     Transform getTransform;
@@ -19,45 +19,46 @@ public class ZeereBeem2 : MonoBehaviour
     void Start()
     {
         target = GameObject.Find("Player");
-        getTransform = transform.Find("Rockon2");
-
+        getTransform = transform.Find("Rockon1");
+        
         sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
+        if(Input.GetKeyDown(KeyCode.B))
         {
             // isCheckの値を反転させる
             onoff = true;
         }
         if (onoff == false)
         {
-            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, transparencyON);
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, transparencyON);//透明化
             Transform myTransform = this.transform;
             Debug.Log(target.transform.position);
             Vector3 pos = target.transform.position;
             pos.y = 0;
-            myTransform.position = pos;
+            myTransform.position = -pos;
         }
-        else if (onoff == true)
+        else if(onoff==true)
         {
-            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, transparencyOFF);
-            passedTimes += Time.deltaTime;
-            if (passedTimes >= firetime)
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, transparencyOFF);//不透明化
+            passedTimes += Time.deltaTime;//時間経過
+            if(passedTimes>=firetime)
             {
                 onoff = false;
                 passedTimes = 0;
-
                 Transform myTransform = this.transform;
                 Vector2 worldPos = myTransform.position;
                 float x = worldPos.x;    // ワールド座標を基準にした、x座標が入っている変数
                 float y = worldPos.y;    // ワールド座標を基準にした、y座標が入っている変数
-                Instantiate(prefab_A, new Vector2(x, y), Quaternion.identity);
-            }
+                Instantiate(prefab_A,new Vector2(x,y), Quaternion.identity);
 
+            }
+           
         }
 
     }
+    
 }
 
