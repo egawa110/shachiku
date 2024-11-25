bee917606201077ba8b60d575b2e3523de7c2cfd
yasutomo <h1202415011@hiratagakuen.onmicrosoft.com>
2024-11-25 16:01:01 +0900

    召喚プログラム移植完了

--- 死神お仕事！/Assets/Hukusumi/Zeere/ZeereCore.cs
+++ 死神お仕事！/Assets/Hukusumi/Zeere/ZeereCore.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class ZeereCore : MonoBehaviour
 {
     Transform Reel;
+    [SerializeField] GameObject Samon;
+    Transform gateTransform;
     public Transform target;
     float passedTimes = 0;
     float coorTime = 0;
     [SerializeField] float speed = 5; // 敵の動くスピード
+    [SerializeField] float RUspeed = 3;
     float ATspeed = 10.0f;
+    float SamonC = 0;
     bool Cool = false;//調整用
     bool AttackLooc = false;//起動用
     bool BusteAttack = false;//突進起動用
+    bool SamonAttack = false;
 
 
 
@@ -20,6 +25,7 @@ public class ZeereCore : MonoBehaviour
     {
         // プレイヤーのTransformを取得（プレイヤーのタグをPlayerに設定必要）
         Reel = GameObject.FindGameObjectWithTag("ZeeReeL").transform;
+        gateTransform = GameObject.FindGameObjectWithTag("SamonTG").transform;
     }
 
     private void Update()
@@ -39,6 +45,13 @@ public class ZeereCore : MonoBehaviour
             // isCheckの値を反転させる
             AttackLooc = !AttackLooc;
             BusteAttack = !BusteAttack;
+            passedTimes = 0;
+            coorTime = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            AttackLooc = !AttackLooc;
+            SamonAttack = !SamonAttack;
             passedTimes = 0;
             coorTime = 0;
         }
@@ -70,13 +83,53 @@ public class ZeereCore : MonoBehaviour
             {
                 // 対象物へのベクトルを算出
                 Vector3 toDirection = target.transform.position - transform.position;
+                //Debug.Log(toDirection);
                 // 対象物へ回転する
                 transform.rotation = Quaternion.FromToRotation(Vector3.up, toDirection);
+
+                //プロト低速ロックオン
+                //if(toDirection.x>0&& transform.localEulerAngles.x-toDirection.x>0)
+                //{
+                //    transform.Rotate(new Vector3(0, 0, 1));
+                //}
             }
             if (passedTimes>=3)
             {
                 Vector3 velocity = gameObject.transform.rotation * new Vector3(0, ATspeed, 0);
                 gameObject.transform.position += velocity * Time.deltaTime;
+            }
+        }
+
+
+        if(SamonAttack==true)
+        {
+            SamonC += Time.deltaTime;//時間経過
+            if (coorTime < 3)
+            {
+                if (SamonC>0.3)
+                {
+                    coorTime += 1;
+                    SamonC = 0;
+                    //Debug.Log("zzz");
+                    // 生成する
+
+                    Transform myTransform = gateTransform.transform;
+                    Vector2 worldPos = myTransform.position;
+                    float x = worldPos.x;
+                    float y = worldPos.y;
+
+                    Instantiate(Samon, new Vector2(x, y), Quaternion.identity);
+
+                    //Instantiate(Samon, gateTransform.position, gateTransform.rotation);
+                }
+            }
+            if(passedTimes>4)
+            {
+                AttackLooc = !AttackLooc;
+                SamonAttack = !SamonAttack;
+                passedTimes = 0;
+                coorTime = 0;
+                SamonC = 0;
             }
         }
         
@@ -91,7 +144,7 @@ public class ZeereCore : MonoBehaviour
             passedTimes = 0;
             coorTime = 0;
             Cool = true;
-            Debug.Log("zzz");
+            
         }
 
     }
