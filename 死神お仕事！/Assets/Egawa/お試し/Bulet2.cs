3d383821249fefcace9a5fb169a64918129bf3b5
egawa <h1202416002@hiratagakuen.onmicrosoft.com>
2024-11-28 12:35:14 +0900

    シーン追加

---
+++ 死神お仕事！/Assets/Egawa/お試し/Bulet2.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Bullet2 : MonoBehaviour
+{
+    [SerializeField] private float speed = 8.0f; //íeÇÃÉXÉsÅ[Éh
+    [SerializeField] private int DeleteTime = 1;
+
+    private PlayerController playcon;
+
+    void Start()
+    {
+        playcon = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        GameObject playerObj = GameObject.Find("Player");
+        if (playerObj.transform.localScale.x >= 0)
+        {
+            Move_R();
+        }
+        else if (playerObj.transform.localScale.x <= 0)
+        {
+            Move_L();
+        }
+
+        Destroy(gameObject, DeleteTime);
+    }
+
+    public void Move_R()
+    {
+        Vector3 bulletPos = transform.position; //Vector3å^ÇÃbulletPosÇ…åªç›ÇÃà íuèÓïÒÇäiî[
+        bulletPos.x -= speed * Time.deltaTime; //xç¿ïWÇ…speedÇâ¡éZÅ@âEå¸Ç´ÅiëOÅj
+        transform.position = bulletPos; //åªç›ÇÃà íuèÓïÒÇ…îΩâfÇ≥ÇπÇÈ
+    }
+    public void Move_L()
+    {
+        Vector3 bulletPos = -transform.position; //Vector3å^ÇÃbulletPosÇ…åªç›ÇÃà íuèÓïÒÇäiî[
+        bulletPos.x -= speed * Time.deltaTime; //xç¿ïWÇ…speedÇâ¡éZÅ@ç∂å¸Ç´Åiå„ÇÎÅj
+        transform.position = bulletPos; //åªç›ÇÃà íuèÓïÒÇ…îΩâfÇ≥ÇπÇÈ
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);//íeÇ™è¡Ç¶ÇÈ
+        }
+        else if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);//íeÇ™è¡Ç¶ÇÈ
+            //Destroy(other.gameObject);//ìGÇ‡è¡Ç¶ÇÈ
+
+        }
+    }
+}
