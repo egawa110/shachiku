301047df71f5ce22d25ddccf876e67213a55c1e7
DESKTOP-M30FNO0\GA1B <h1202419013@hiratagakuen.onmicrosoft.com>
2024-12-05 10:49:16 +0900

    testシーンにてダメージボタンを押すとHPバーが減るようになった。

--- 死神お仕事！/Assets/Shishido/script/Player_s.cs
+++ 死神お仕事！/Assets/Shishido/script/Player_s.cs
@@ -1,34 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
-using UnityEngine.SocialPlatforms;
+using UnityEngine.Android;
 
 public class Player_s : MonoBehaviour
 {
-    
-    Rigidbody2D rbody;              //Rigidbody2D型の変数
-    float axisH = 0.0f;             //入力
-    public float speed = 3.0f;      //移動速度
-  
-    public float jump = 9.0f;       //ジャンプ力
-    public LayerMask groundLayer;   //着地できるレイヤー
-    bool goJump = false;            //ジャンプ開始フラグ
-    
-    
-    // アニメーション対応
-    Animator animator; // アニメーター
+
+    Rigidbody2D rbody;                //Rigidbody2D型の作成
+    float axisH = 0.0f;               //入力
+    public float speed = 3.0f;        //移動速度
+
+    public float jump = 9.0f;         //ジャンプ力
+    public LayerMask groundLayer;     //着地できるレイヤー
+    bool goJump = false;              //ジャンプ開始フラグ
+
+
+    //アニメーション対応
+    Animator animator; //アニメーター
     public string stopAnime = "PlayerStop";
     public string moveAnime = "PlayerMove";
     public string jumpAnime = "PlayerJump";
     public string goalAnime = "PlayerGoal";
     public string deadAnime = "PlayerOver";
-   
+
     string nowAnime = "";
     string oldAnime = "";
-    
-    public static string gameState = "playing"; // ゲームの状態
 
-    
+    public static string gameState = "playing";// ゲームの状態
+
+
     //追加
     public int ALL_SOUL = 0;      //1ステージで取得したすべての魂
 
@@ -37,22 +39,26 @@ public class Player_s : MonoBehaviour
     [SerializeField] private Transform attackPoint; //アタックポイントを格納
 
     [SerializeField] private float attackTime = 0.2f; //攻撃間隔
+    public float fireSpeed = 8.0f;
+
     private float currentAttackTime;                  //攻撃の間隔を管理
     private bool canAttack;                           //攻撃可能状態かを指定するフラグ
-   
+
+
     public int HP_P = 4;      //プレイヤーの体力
-    bool inDamage = false;  //ダメージ中のフラグ
+    private bool inDamage = false;  //ダメージ中のフラグ
 
     // サウンド再生
-    private AudioSource audioSource;
-    public AudioClip Jump_SE;
-    public AudioClip Damage_SE;
-    public AudioClip GetSoul_SE;
-    public AudioClip Attack_SE;
-    public AudioClip Switch_Act_SE;
-    public AudioClip Clear_SE;
-    public AudioClip Over_SE;
+    //private AudioSource audioSource;
+    //public AudioClip Jump_SE;
+    //public AudioClip Damage_SE;
+    //public AudioClip GetSoul_SE;
+    //public AudioClip Attack_SE;
+    //public AudioClip Switch_Act_SE;
+    //public AudioClip Clear_SE;
+    //public AudioClip Over_SE;
 
+    private GaugeController gauge;//GaugeControllerスクリプト用の変数
 
     // Start is called before the first frame update
     void Start()
@@ -60,22 +66,24 @@ public class Player_s : MonoBehaviour
         //FPSを60に固定
         Application.targetFrameRate = 60;
 
-        // Rigidbody2Dを取ってくる
-        rbody = this.GetComponent<Rigidbody2D>();   //Rigidbody2Dを取ってくる
-        animator = GetComponent<Animator>();        //Animator を取ってくる
-        nowAnime = stopAnime;                       //停止から開始する
-        oldAnime = stopAnime;                       //停止から開始する
-        
-        gameState = "playing";                      // ゲーム中にする
+        //Rigidbod2Dを取ってくる
+        rbody = this.GetComponent<Rigidbody2D>(); //Rigidbody2Dを取ってくる
+        animator = GetComponent<Animator>();      //Animatorを取ってくる
+        nowAnime = stopAnime;                     //停止から開始する
+        oldAnime = stopAnime;                     //停止から開始する
 
-        audioSource = GetComponent<AudioSource>();
+        gameState = "playing";//ゲーム中にする
+
         currentAttackTime = attackTime; //currentAttackTimeにattackTimeをセット。
+        //audioSource = GetComponent<AudioSource>();
+
+        gauge = GetComponent<GaugeController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.timeScale==0)
+        if (Time.timeScale == 0)
         {
             return;
         }
@@ -83,15 +91,16 @@ public class Player_s : MonoBehaviour
         {
             return;
         }
-        
-        //水平方向の入力をチェックする
+
+        //水平方向の入力をチェック
         axisH = Input.GetAxisRaw("Horizontal");
-        
+
         //向きの調整
         if (axisH > 0.0f)
         {
             transform.localScale = new Vector2(1, 1);
         }
+
         else if (axisH < 0.0f)
         {
             transform.localScale = new Vector2(-1, 1);
@@ -129,12 +138,23 @@ public class Player_s : MonoBehaviour
             }
             return; // ダメージ中は操作による移動をさせない
         }
+        //CircleCast
+
+        /*
+                 bool onGround = Physics2D.CircleCast(transform.position,    //発射位置
+                                             1.5f,                  //円の半径
+                                             Vector2.down,          //発射方向
+                                             0.0f,                  //発射距離
+                                             groundLayer);          //検出するレイヤー
+
+         */
+
 
         //地上判定
         bool onGround = Physics2D.CircleCast(transform.position,    //発射位置
-                                             1.8f,                  //円の半径
+                                             0.7f,                  //円の半径
                                              Vector2.down,          //発射方向
-                                             0.0f,                  //発射距離
+                                             1.0f,                  //発射距離
                                              groundLayer);          //検出するレイヤー
         if (onGround || axisH != 0)
         {
@@ -145,10 +165,11 @@ public class Player_s : MonoBehaviour
         {
             //地面の上でジャンプキーが押された
             //ジャンプさせる
-            Vector2 jumpPw = new Vector2(0, jump);          //ジャンプさせるベクトルを作る
+            Vector2 jumpPw = new(0, jump);                  //ジャンプさせるベクトルを作る
             rbody.AddForce(jumpPw, ForceMode2D.Impulse);    //瞬間的な力を加える
             goJump = false;
-            audioSource.PlayOneShot(Jump_SE);
+            //ジャンプ音を鳴らす
+            //udioSource.PlayOneShot(Jump_SE);
         }
         //アニメーション更新
         if (onGround)
@@ -181,6 +202,7 @@ public class Player_s : MonoBehaviour
     {
         goJump = true;                      //ジャンプフラグを立てる
     }
+
     //攻撃
     public void Attack()
     {
@@ -191,71 +213,101 @@ public class Player_s : MonoBehaviour
             canAttack = true; //指定時間を超えたら攻撃可能にする
         }
 
-        if (Input.GetKeyDown(KeyCode.Z)) //Zキーを押したら
+        if (Input.GetKeyDown(KeyCode.K)) // Kキーを押したら
         {
+            //Debug.Log("ZZZ");
+
             if (canAttack)
             {
                 GameObject playerObj = GameObject.Find("Player");
                 if (playerObj.transform.localScale.x >= 0)
                 {
-                    CreateBullet();
+                    //CreateBullet_R();
+                    Vector2 pos = new Vector2(attackPoint.position.x,
+                            attackPoint.position.y);
+                    GameObject obj = Instantiate(bullet, pos, Quaternion.identity);
+                    //方針が向いてる方向に発射する
+                    Rigidbody2D rbody = obj.GetComponent<Rigidbody2D>();
+                    float angleZ = transform.localEulerAngles.z;
+                    float x = Mathf.Cos(angleZ * Mathf.Deg2Rad);
+                    float y = Mathf.Sin(angleZ * Mathf.Deg2Rad);
+                    Vector2 v = new Vector2(x, y) * fireSpeed;
+                    rbody.AddForce(v, ForceMode2D.Impulse);
                 }
+                else if (playerObj.transform.localScale.x <= 0)
+                {
+                    Vector2 pos = new Vector2(attackPoint.position.x,
+                            attackPoint.position.y);
+                    GameObject obj = Instantiate(bullet, pos, Quaternion.identity);
+                    //方針が向いてる方向に発射する
+                    Rigidbody2D rbody = obj.GetComponent<Rigidbody2D>();
+                    float angleZ = transform.localEulerAngles.z;
+                    float x = Mathf.Cos(angleZ * Mathf.Deg2Rad);
+                    float y = Mathf.Sin(angleZ * Mathf.Deg2Rad);
+                    Vector2 v = new Vector2(-x, y) * fireSpeed;
+                    rbody.AddForce(v, ForceMode2D.Impulse);
+                }
+                canAttack = false; //攻撃フラグをfalseにする
+                attackTime = 0f;　 //attackTimeを0に戻す
             }
+
         }
     }
 
-    public void CreateBullet()
-    {
-        //第一引数に生成するオブジェクト、第二引数にVector3型の座標、第三引数に回転の情報
-        Instantiate(bullet, attackPoint.position, Quaternion.identity);
-        canAttack = false; //攻撃フラグをfalseにする
-        attackTime = 0f;　 //attackTimeを0に戻す
-    }
-
-    // 接触開始
+    //接触開始
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Goal")
         {
-            Goal();        // ゴール！！
+            Goal();
         }
-        else if (collision.gameObject.tag == "Dead")
+        else if (collision.gameObject.tag == "Dead" || collision.gameObject.tag == "ZeereCore")
         {
-            GameOver();     // ゲームオーバー
+            GameOver(); //ゲームオーバー
         }
-
-        //追加
         else if (collision.gameObject.tag == "Soul")
         {
             //魂取得する
             Souls item = collision.gameObject.GetComponent<Souls>();
             ALL_SOUL += item.soul_one;
-            audioSource.PlayOneShot(GetSoul_SE);
+            //音を鳴らす
+            //audioSource.PlayOneShot(GetSoul_SE);
             // 削除する
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.tag == "Enemy")
         {
             GetDamage(collision.gameObject);
-            audioSource.PlayOneShot(Damage_SE);
+
+            //gauge.BeInjured(1);
+
+            //敵に当たった時に音を鳴らす
+            //audioSource.PlayOneShot(Damage_SE);
+        }
+        else if (collision.gameObject.tag == "Switch")
+        {
+            // スイッチに触れたら音を鳴らす
+            // audioSource.PlayOneShot(Switch_Act_SE);
         }
     }
 
-    //ダメージ
     void GetDamage(GameObject enemy)
     {
         if (gameState == "playing")
         {
             HP_P--; //hpが減る
+
             if (HP_P > 0)
             {
                 //移動停止
                 rbody.velocity = new Vector2(0, 0);
-                //敵キャラの反対方向にノックバックさせる
-                Vector3 v = (transform.position - enemy.transform.position).normalized;　rbody.AddForce(new Vector2(v.x * 4, v.y * 4), ForceMode2D.Impulse);
+                //敵キャラの反対方向にヒットバックさせる
+                Vector3 v = (transform.position - enemy.transform.position).normalized; rbody.AddForce(new Vector2(v.x * 4, v.y * 4), ForceMode2D.Impulse);
                 //ダメージフラグ　ON
                 inDamage = true;
-                Invoke("DamageEnd", 0.25f);
+                Invoke(nameof(DamageEnd), 0.25f);
+
+                gauge.BeInjured(1);
             }
             else
             {
@@ -277,8 +329,11 @@ public class Player_s : MonoBehaviour
         animator.Play(goalAnime);
         gameState = "gameclear";
         GameStop();             // ゲーム停止
+        //音楽を鳴らす
+        //audioSource.PlayOneShot(Clear_SE);
+
     }
-    
+
     // ゲームオーバー
     public void GameOver()
     {
@@ -286,17 +341,21 @@ public class Player_s : MonoBehaviour
         gameState = "gameover"; GameStop();
         // ゲーム停止（ゲームオーバー演出）
         // プレイヤー当たりを消す
-        GetComponent<CapsuleCollider2D>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
         // プレイヤーを上に少し跳ね上げる演出
         rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        //音楽を鳴らす
+        //audioSource.PlayOneShot(Over_SE);
     }
 
-    // ゲーム停止
+
+    //ゲーム停止
     void GameStop()
     {
-        // Rigidbody2Dを取ってくる
+        //Rigidbody2Dを取ってくる
         Rigidbody2D rbody = GetComponent<Rigidbody2D>();
-        // 速度を 0 にして強制停止
+        //速度を０にして強制停止
         rbody.velocity = new Vector2(0, 0);
     }
+
 }
