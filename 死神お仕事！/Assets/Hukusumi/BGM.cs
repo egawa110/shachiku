2eca96c2b7b5e086a799cbe81c9dad5421e04ae2
yasutomo <h1202415011@hiratagakuen.onmicrosoft.com>
2024-12-12 11:05:51 +0900

    BGM選定中

---
+++ 死神お仕事！/Assets/Hukusumi/BGM.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    //public AudioClip intro;
+    //public AudioClip loop;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    //イントロ部分の再生開始
+    //    intro.PlayScheduled(AudioSettings.dspTime);
+
+    //    //イントロ終了後にループ部分の再生を開始
+    //    loop.PlayScheduled(AudioSettings.dspTime + ((float)intro.clip.samples / (float)intro.clip.frequency));
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
