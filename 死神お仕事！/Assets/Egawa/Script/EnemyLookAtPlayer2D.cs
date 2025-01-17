3d55855f843faf8cf4d925820cd8123db32d3e40
egawa <h1202416002@hiratagakuen.onmicrosoft.com>
2025-01-17 09:25:08 +0900

    敵挙動追加中

---
+++ 死神お仕事！/Assets/Egawa/Script/EnemyLookAtPlayer2D.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLookAtPlayer2D : MonoBehaviour
+{
+    public Transform player; // プレイヤーのTransformを指定
+
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // 2Dゲーム用にZ軸の回転のみを設定
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
