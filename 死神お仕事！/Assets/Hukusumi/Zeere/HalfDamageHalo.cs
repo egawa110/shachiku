1f0afdca3125af417104f4eb0552a007914a8d7c
yasutomo <h1202415011@hiratagakuen.onmicrosoft.com>
2025-01-31 12:26:51 +0900

    輪っかチカチカ レッシュンうろうろ

--- 死神お仕事！/Assets/Hukusumi/Zeere/HalfDamageHalo.cs
+++ 死神お仕事！/Assets/Hukusumi/Zeere/HalfDamageHalo.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class HalfDamageHalo : MonoBehaviour
 {
+    int rnd;//乱数
+    bool inDamage = false;
+    bool HalfF = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,43 @@ public class HalfDamageHalo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //乱数で一瞬消える感じでおなしゃす
+        if (HalfF)
+        {
+            //乱数で一瞬消える感じでおなしゃす
+            rnd = Random.Range(0, 100);//乱数
+            if (inDamage)
+            {
+                //ダメージ中、点滅させる
+                float val = Mathf.Sin(Time.time * 50);
+                if (val > 0)
+                {
+                    //スプライトを表示
+                    gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                }
+                else
+                {
+                    //スプライトを非表示
+                    gameObject.GetComponent<SpriteRenderer>().enabled = false;
+                }
+                rnd = 100;
+            }
+            if (rnd == 0)
+            {
+                inDamage = true;
+                Invoke(nameof(DamageEnd), 0.25f);
+            }
+        }
+
+    }
+
+    //ダメージ終了
+    void DamageEnd()
+    {
+        inDamage = false; // ダメージフラグOFF
+        gameObject.GetComponent<SpriteRenderer>().enabled = true; // スプライトを元に戻す
+    }
+    void HarfFON()
+    {
+        HalfF = true;
     }
 }
--- 死神お仕事！/Assets/Hukusumi/Zeere/Lessyn_Rifgo.cs
+++ 死神お仕事！/Assets/Hukusumi/Zeere/Lessyn_Rifgo.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class Lessyn_Rifgo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject objPrefab;   //発生させるPrefabデータ
+    Transform gateTransform;       //発射口のTransform
+    Rigidbody2D rbody;                //Rigidbody2D型の作成
+    Transform Zeere;
+    public float speed = 3.0f;  //移動速度
+    public LayerMask groundLayer;      //地面レイヤー
+    public float fireSpeed = 4.0f; //発射速度
+
+    float passedTimes = 0;
+    public float firetime = 3.0f;//発射
+
+    private AudioSource audioSource;
+    public AudioClip Fire_SE;
+
+    GameObject Zyuto;
+
     void Start()
     {
-        
+        gateTransform = transform.Find("gate");
+        Zeere = GameObject.FindGameObjectWithTag("ZeereCore").transform;
+        //rbody = this.GetComponent<Rigidbody2D>(); //Rigidbody2Dを取ってくる
+        audioSource = GetComponent<AudioSource>();
+
+        Zyuto = this.transform.Find("name").gameObject;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Transform myTransform = this.transform;
+        Vector2 worldPos = myTransform.position;
+        passedTimes += Time.deltaTime;//時間経過
+            transform.position = Vector2.MoveTowards(
+                   transform.position,
+                   new Vector2(Zeere.position.x, worldPos.y),
+                   speed * Time.deltaTime);
+           
+        if (gameObject.GetComponent<SpriteRenderer>().enabled == false)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
     }
+
+    void FixedUpdate()
+    {
+        //地上判定
+        bool onGround = Physics2D.CircleCast(transform.position, //発射位置
+                                             1.8f,               //円の半径
+                                             Vector2.down,       //発射方向
+                                             0.0f,               //発射距離
+                                             groundLayer);       //検出するレイヤー
+    }
+
+    //接触
+    private void OnTriggerEnter2D(Collider2D other) //ぶつかったら消える命令文開始
+    {
+        if (other.CompareTag("KinKill"))
+        {
+            Destroy(gameObject);//このゲームオブジェクトを消滅させる
+        }
+    }
+
 }
+
