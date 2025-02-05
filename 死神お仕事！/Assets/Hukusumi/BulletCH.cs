74097a6bd4582209f5df6f375c20778a84ff3669
yasutomo <h1202415011@hiratagakuen.onmicrosoft.com>
2025-02-05 11:39:58 +0900

    回復弾貫通化＆自己消去形修正

---
+++ 死神お仕事！/Assets/Hukusumi/BulletCH.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCH : MonoBehaviour
+{
+    public float deleteTime = 3.0f; //削除する時間指定
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, deleteTime); //削除設定
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);//このゲームオブジェクトを消滅させる
+        }
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);//このゲームオブジェクトを消滅させる
+        }
+        if (other.CompareTag("KinKill"))
+        {
+            Destroy(gameObject);//このゲームオブジェクトを消滅させる
+        }
+
+    }
+}
